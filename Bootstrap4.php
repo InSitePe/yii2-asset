d1fa355501c9852acefca59453e8cb302860723d
Jose Nolberto Vilchez Moreno <jose.vilchez.m@upch.pe>
2017-10-20 18:16:54 -0500

    Agregando Bootstrap 4

---
+++ Bootstrap4.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace insite\asset;
+
+class Bootstrap4 extends \Yii\web\AssetBundle {
+
+    /**
+     * @inherit
+     */
+    public $sourcePath = '@vendor/twbs/bootstrap/dist';
+
+    /**
+     * @inherit
+     */
+    public $css = [
+        'css/bootstrap.min.css',
+    ];
+
+    /**
+     * @inherit
+     */
+    public $js = [
+        'js/bootstrap.min.js'
+    ];
+
+}
